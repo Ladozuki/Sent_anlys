72ef8e814222a6a27525451d9aaf368fba398bbd
Ladozuki <leokusanya@outlook.com>
2025-01-25 23:31:42 +0000

    pdf generation file

--- maritimepw/generate_report.py
+++ maritimepw/generate_report.py
@@ -21,94 +21,6 @@ absolute_paths = {
     "BZ": encode_image(os.path.join(base_dir, "images/BZ=F_price_comparison.png")),
 }
 
-# Compact table HTML template
-# compact_table_template = """
-# <!DOCTYPE html>
-# <html lang="en">
-# <head>
-#     <meta charset="UTF-8">
-#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-#     <title>{{ title }}</title>
-#     <style>
-#         @page {
-#             size: A4;
-#             margin: 20px;
-#         }
-#         body {
-#             font-family: Arial, sans-serif;
-#             margin: 5px;
-#             line-height: 1.4;
-#             font-size: 10px;
-#         }
-#         h1 {
-#             text-align: center;
-#             color: #333;
-#             margin-top: 5px 0;
-#             font-size: 24px;
-#         }
-#         h2 {
-#             color: #444;
-#             border-bottom: 1px solid #ccc;
-#             margin-top: 10px;
-#             font-size: 14px;
-#         }
-#         p {
-#             padding: 0;
-#             margin-top: 1px;
-#         }
-#         .section {
-#             display: flex;
-#             justify-content: space-between;
-#             align-items: flex-start;
-#             gap: 35px;
-#             margin-bottom: 15px;
-#         }
-#         .text {
-#             flex: 3;
-#             margin-right: 10px;
-#         }
-#         .charts {
-#             flex: 4;
-#             margin-top: 0;
-#         }
-#         img {
-#             display: block;
-#             margin: 10px auto;
-#             max-width: 50%;
-#         }
-#     </style>
-# </head>
-# <body>
-#     <h1>{{ title }}</h1>
-#     <p><strong>Date:</strong> {{ date }}</p>
-    
-#     <!-- Display the map -->
-#     <h2>Strategic Tanker Routes</h2>
-#     <img src="data:image/png;base64,{{ map_image }}" alt="Strategic Tanker Routes Map" style="width: 80%; max-width: 300px; margin: 20px auto;">
-
-#     <h2>Dangote Refinery</h2>
-#     <div class="section">
-#         <div class="text">
-#             <p><strong>Capacity & Impact:</strong> Operating at ~50%, the refinery processes 650,000 barrels/day, exporting petrol, diesel, and jet fuel. Key destinations include Europe and regional hubs like Ghana and South Africa, challenging traditional suppliers.</p>
-#             <p><strong>Strategic Expansion:</strong> Plans include building 6.3 million liters of storage capacity to enhance export capabilities.</p>
-#         </div>
-#         <div class="charts">
-#             <h3>Global Impact</h3>
-#             <p><strong>Oil Price Dynamics:</strong> Increased local refining capacity reduces Nigeria’s import requirements, influencing global product tanker flows.</p>
-#         </div>
-#     </div>
-
-#     <h2>Graphs</h2>
-#     {% for image in sigc_graphs %}
-#         <img src="data:image/png;base64,{{ image }}" alt="Refinery Graph" style="max-width:100%; margin: 20px auto;">
-#     {% endfor %}
-#     {% for image in bzcl_graphs %}
-#         <img src="data:image/png;base64,{{ image }}" alt="Market Graph" style="max-width:100%; margin: 20px auto;">
-#     {% endfor %}
-# </body>
-# </html>
-# """
-
 #Compact table HTML template
 compact_table_template = """
 <!DOCTYPE html>
@@ -125,20 +37,20 @@ compact_table_template = """
         body {
             font-family: Arial, sans-serif;
             margin: 5px
-            line-height: 1.4;
+            line-height: 1.3;
             font-size: 10px;
         }
         h1 {
             text-align: center;
             color: #333;
             margin-top: 5px 0;
-            font-size: 24px;
+            font-size: 20px;
         }
         h2 {
             color: #444;
             border-bottom: 1px solid #ccc;
             margin-top: 10px;
-            font-size: 14px;
+            font-size: 13px;
         }
         p {
             padding: 0;
@@ -151,7 +63,7 @@ compact_table_template = """
             align-items: flex-start;
             gap: 15px;
             width: 100%;
-            margin-bottom: 10px;
+            margin-bottom: 5px;
         }
         .text {
             flex: 3.5;
@@ -182,11 +94,11 @@ compact_table_template = """
             width: 100%;
             border-collapse: collapse;
             margin: 0 auto;
-            font-size: 8px;
+            font-size: 10px;
             margin-top: 3px;
         }
         table, th, td {
-            border: 1px solid #ccc;
+            border: 1px solid #;
         }
         th, td {
             padding: 2px;
@@ -195,7 +107,8 @@ compact_table_template = """
         }
         th {
             background-color: #f4f4f4;
-            font-size: 8px;
+            font-weight: bold;
+            font-size: 10px;
         }
         .routes th:nth-child(1),
         .routes td:nth-child(1) {
@@ -230,47 +143,57 @@ compact_table_template = """
         }
         .cards {
             display: flex;
-            gap: 15px;
-            margin: 10px 0;
+            gap: 10px;
+            margin: 5px 0;
             justify-content: space-around;
             padding: 5px 0;}
         .card {
-            flex: 1 1 calc(20% - 10px); /* Adjust width of each card */
-            padding: 10px;
+            flex: 1 1 calc(15% - 8px); /* Adjust width of each card */
+            flex-wrap: wrap;
+            padding: 5px;
             border: 1px solid #ccc;
-            border-radius: 8px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            border-radius: 6px;
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
             text-align: center;
-            background-color: #f9f9f9;}
+            background-color: #000;
+            color: #fff}
         .card h3 {
-            font-size: 12px;
-            margin-bottom: 5px;
-            color: #444;}
-        .card p {
             font-size: 10px;
-            margin: 3px 0;}
+            margin-bottom: 4px;
+            color: #fff;}
+        .card p {
+            font-size: 8px;
+            margin: 3px 0;
+            color: #ccc;}
         .card strong {
-            font-size: 14px;
-            color: #222;}
+            font-size: 10px;
+            color: #fff;}
 </style>
 </head>
 <body>
     <h1>{{ title }}</h1>
+    <p style="text-align: left; font-size: 10px; font-weight: bold;">22 January 2025 </p>
 
     <div class="section">
         <div class="text">
-            <p><strong>Capacity & Impact:</strong> The Dangote Refinery, operational since December 2023, is the largest single-train refinery in the world, with a capacity of 650,000 barrels per day. Its production has significantly reduced Nigeria's reliance on imported refined products, particularly gasoline from Europe. This shift is disrupting traditional gasoline trade routes and challenging long-established suppliers.</p>
-            <p><strong>Foreign Exchange & Export Earnings:</strong> Reducing refined product imports, the country can conserve foreign exchange. Additionally, with domestic demand met, Nigeria is positioned to export refined products, boosting foreign exchange earnings, improving the trade balance, and stabilizing the naira.</p>
-            <p><strong>Strategic Expansion:</strong> Ongoing crude oil supply constraints due to limited availability from NNPC contributed to consturction of 6.3 million liters of additional storage capacity. Nigeria’s crude production has recovered from 931k bpd in 2022 to 1.5million bpd in 2024,  with the potential of 1.8m in 2025.</p>
-            <p><strong>Energy Diversification:</strong> Achieving energy self-sufficiency within the next decade will require transitioning from petrol (PMS) to gas as the primary energy source, incorporating renewables, and leveraging resources like lithium for sustainability. Transparency in the downstream oil and gas sector remains a critical challenge.</p>
-           
-            <p>{{ overview }}</p> 
+            <p><strong>Capacity & Impact:</strong> The <strong>Dangote Refinery</strong>, operational since December 2023, is the <strong>largest single-train refinery in the world</strong>, with a capacity of <strong>650,000 barrels per day</strong>. Its production has significantly reduced Nigeria's reliance on <strong>imported refined products</strong>, particularly gasoline from Europe. This shift is <strong>disrupting traditional gasoline trade routes</strong> and challenging long-established suppliers.</p>
+
+            <p><strong>Foreign Exchange & Export Earnings:</strong> Reducing refined product imports, the country can <strong>conserve foreign exchange</strong>. Additionally, with <strong>domestic demand met</strong>, Nigeria is positioned to export refined products, <strong>stabilizing the naira</strong> in the process.</p>
+
+            <p><strong>Strategic Expansion:</strong> Ongoing crude oil supply constraints due to limited availability from NNPC contributed to the construction of <strong>6.3 million liters of additional storage capacity</strong>. Nigeria’s crude production has recovered from <strong>931k bpd in 2022</strong> to <strong>1.5 million bpd in 2024</strong>, with the potential of <strong>1.8m in 2025</strong>.</p>
+            
+            <h2 style="text-align: center; margin-top: 20px;">Market Overview</h2>
+
+            <p>The tanker market saw varied activity this week. <strong>VLCC</strong> and <strong>Suezmax</strong> earnings rose on <strong>Middle East Gulf</strong> and <strong>West Africa routes</strong>, while <strong>Aframax</strong> and <strong>MR tankers</strong> remained steady with balanced demand in the <strong>Atlantic</strong> and <strong>Asia-Pacific regions</strong>.</p>
+
+            <p><strong>Key Indices:</strong> The <strong>Baltic Dirty Tanker Index (BDTI)</strong> gained <strong>34 points</strong> to <strong>855</strong>, while the <strong>Baltic Clean Tanker Index (BCTI)</strong> rose <strong>11 points</strong> to <strong>640</strong>. <strong>Sale and purchase activity softened</strong>, <strong>asset and recycling prices declined</strong>, and <strong>bunker prices saw moderate increases</strong> across major hubs. <strong>FFA volumes contracted</strong> in both <strong>clean</strong> and <strong>dirty segments</strong>.</p>
 
             <table class="routes">
+            <h3 style="text-align: center; margin-top: 10px;">Largest changess</h3>
             <thead>
                 <tr>
                     <th>Route</th>
-                    <th>Highest TCE Changes - Description</th>
+                    <th>Description</th>
                     <th>Worldscale</th>
                     <th>TCE ($/day)</th>
                     <th>+/-</th>
@@ -297,6 +220,7 @@ compact_table_template = """
         </table>
 
         <div class="demarcation"></div>
+        <h3 style="text-align: center; margin-top: 16px;">Dirty Routes</h3>
         <table class="routes">
             <thead>
                 <tr>
@@ -327,6 +251,7 @@ compact_table_template = """
             </tbody>
         </table>
         <div class="demarcation"></div>
+        <h3 style="text-align: center; margin-top: 10px;">Clean Routes</h3>
                 <table class="routes">
             <thead>
                 <tr>
@@ -359,44 +284,50 @@ compact_table_template = """
 
         <div class="cards">
         <div class="card">
-            <h3>Suezmax TCE</h3>
-            <p><strong>$30,422/day</strong></p>
-            <p style="color: green;">+385 $/day &#9650;</p>
+            <h3>Tanker Recycling Index </h3>
+            <p><strong>11,437</strong></p>
+            <p style="color: red;">-73 &#9660;</p>
         </div>
         <div class="card">
-            <h3>Aframax TCE</h3>
-            <p><strong>$25,068/day</strong></p>
-            <p style="color: green;">+355 $/day &#9650;</p>
+            <h3>Tanker Sale and Purchase Index </h3>
+            <p><strong>7,623</strong></p>
+            <p style="color: red;">-3 &#9660;</p>
         </div>
         <div class="card">
-            <h3>VLCC TCE</h3>
-            <p><strong>$57,025/day</strong></p>
-            <p style="color: green;">+1717 $/day &#9650;</p>
+            <h3>Tanker New Building Index </h3>
+            <p><strong>7,753</strong></p>
+            <p style="color: red;">-37 &#9660;</p>
         </div>
-        <div class="card">
-            <h3>VLCC OpEX</h3>
-            <p><strong>$8,080/day</strong></p>
-            <p style="color: green;">+102 $/day &#9650;</p>
-        </div>
+        
+        
+        
     </div>
     </div>
+    
         <div class="charts">
-            <p><strong>Regional & Global Dynamics. Decline in MR Tanker Imports:</strong> Reduced reliance on gasoline imports will impact traditional MR flows. Foreign refineries, especially in Europe, must explore new markets for their products, potentially leading to inventory buildup and price disruptions.</p>
-            <p><strong>Rising Export Demand:</strong> Increased production capacity is driving regional exports to West-African Coast, South Africa, and abroad, boosting demand for Handysize and MR tankers. This dynamic enhances Nigeria's position as a regional energy hub.</p>
-            <p><strong>Global Impact:</strong> New export routes will emerge as local refining capacity reduces Nigeria’s import dependency. This shift reshapes global tanker flows, increases competition in other markets, et al</p>
-            <p><strong>Geopolitical Considerations:</strong> The Dangote Refinery strengthens Nigeria’s role in the global oil and gas ecosystem, supporting regional energy security and fostering trade ties. Resistance to this change is anticipated due to its disruption of long-established trade flows.</p>
-            <h2>Global Bunker Prices</h2>
+           <p><strong>Regional & Global Dynamics:</strong> Reduced reliance on gasoline imports will <strong>impact traditional MR flows</strong>. Foreign refineries, especially in Europe, must <strong>explore new markets</strong> for their products, potentially leading to <strong>inventory buildup</strong> and price disruptions.</p>
+            <p><strong>Rising Export Demand:</strong> Increased production capacity is <strong>driving regional exports</strong> to the West-African Coast, South Africa, and abroad, boosting demand for <strong>Handysize</strong> and <strong>MR tankers</strong>. This dynamic enhances Nigeria's position as a <strong>regional energy hub</strong>.</p>
+            <p><strong>Global Impact:</strong> New export routes will emerge as local refining capacity reduces Nigeria’s <strong>import dependency</strong>. This shift reshapes <strong>global tanker flows</strong>, increases competition in other markets, and more.</p>
+            <p><strong>Energy Diversification:</strong> Achieving energy self-sufficiency within the next decade will require transitioning from <strong>petrol (PMS)</strong> to <strong>gas</strong> as the primary energy source, incorporating <strong>renewables</strong>, and leveraging resources like <strong>lithium</strong> for sustainability.</p>
+            
+            <h2 style="text-align: center; margin-top: 10px;">Global Bunker Prices</h2>
             <img src="data:image/png;base64,{{ map_image }}" 
              alt="Strategic Tanker Routes Map" 
-             style="max-width: 90%; height: auto; margin: 10px auto; border: 1px solid #ccc;">
+             style="max-width: 100%; height: auto; margin: 20px auto; border: 1px solid #ccc;">
 
             {% for image in bzcl_graphs %}
                 <img src="data:image/png;base64,{{ image }}" 
                     alt="Market Graph" 
-                        style="width: 100%; max-width: 600px; max-height: 400px; object-fit: contain; margin: 5px auto; border: 1px solid #ccc;">
+                        style="width: 100%; max-width: 600px; max-height: 300px; object-fit: contain; margin: 5px auto; border: 1px solid #ccc;">
         {% endfor %}
+        
         </div>
     </div>
+
+
+    <footer style="text-align: center; font-size: 8px; color: #555; margin-top: px; border-top: 1px solid #ccc; padding-top: 5px;">
+        <p>© 2025 Lado Limited. All Rights Reserved.</p>
+    </footer>
 
 </body>
 </html>
@@ -432,12 +363,6 @@ full_route_data = [
 updated_report_data_with_all_routes = {
     "title": "Maritime Per Week",
     "date": "21 01 2025",
-    "overview": """
-    The tanker market saw dynamic activity this week, with VLCC and Suezmax earnings on Middle East Gulf and West Africa routes experiencing significant gains. Aframax and MR tankers showed steadier movements, supported by balanced demand in the Atlantic and Asia-Pacific regions.
-
-    Key Index Movements: The Baltic Dirty Tanker Index (BDTI) rose 34 points to 855, reflecting robust crude demand. The Baltic Clean Tanker Index (BCTI) gained 11 points to 640, underscoring resilience in clean product markets.
-
-    """,
     "highest_changes": sorted(full_route_data, key=lambda x: x["Change (TCE)"], reverse=True)[:5],
     "remaining_routes": full_route_data,
     "sigc_graphs": [absolute_paths["SI"], absolute_paths["GC"],],
